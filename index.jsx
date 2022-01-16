2ea9131ea0b17069f7b323913d241f9fbd280179
MarcusKri <marcus-kristoffersen@outlook.com>
2022-01-16 19:56:16 +0000

    List movies in <ListMovies> component

--- index.jsx
+++ index.jsx
@@ -2,6 +2,20 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import {Routes, Route, Link, BrowserRouter} from "react-router-dom";
 
+
+const movies = [
+    {
+        title: "Tittel på film 1",
+        plot: "Filmen handler om en kjekkas med navn Marcus, som blir veldig stor og sterk",
+        year: 1997
+    },
+    {
+        title: "De Urørlige",
+        plot: "En funksjonshemmed mann finner en gangster som arbeidstaker, og det skjer mye kaos",
+        year: 2010
+    }
+];
+
 function FrontPage() {
     /* Link til sidene */
     return <div>
@@ -13,11 +27,24 @@ function FrontPage() {
     </div>
 }
 
+function ListMovies() {
+    return <div>
+        <h1>List movies</h1>
+
+            {movies.map(m =>
+                <>
+                    <h2>{m.title} ({m.year})</h2>
+                    <div>{m.plot}</div>
+                </>
+            )}
+    </div>;
+}
+
 function Application() {
     return <BrowserRouter>
         <Routes>
             <Route path="/" element={<FrontPage />} />
-            <Route path="/movies" element={<h1>List movies</h1>} />
+            <Route path="/movies" element={<ListMovies/>} />
             <Route path="/movies/new" element={<h1>New movie</h1>} />
         </Routes>
     </BrowserRouter>;
